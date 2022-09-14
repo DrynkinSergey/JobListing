e784d4a91fd16593b941d7af10a8c1f5ba9e8665
Sergey <sergsmile29@gmail.com>
2022-09-15 00:30:27 +0300

    added store and useSelector for view data

---
+++ src/store/root-reducer.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux'
+import { positionReducer } from './positions/position-reducer'
+export const rootReducer = combineReducers({
+	positions: positionReducer,
+})
